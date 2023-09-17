2f5a34cad20a16e4328de02137dde82bc35fb02d
Sydney Melendres <sydneymelendres@gmail.com>
2023-09-17 23:45:54 +1000

    SDS-108 - Added extra alerts (not implemented yet)

---
+++ src/components/alerts/WarningTwo.tsx
@@ -0,0 +1,80 @@
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Progress,
+  Box,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const WarningTwo = () => {
+  const [showAlert, setShowAlert] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = 10; // update every 10ms
+    const totalDuration = 5000; // 5 seconds in total
+
+    // Initialize the progress state to totalDuration
+    setProgress(totalDuration);
+
+    const timer = setInterval(() => {
+      setProgress((prev) => {
+        const nextValue = prev - interval;
+        if (nextValue <= 0) {
+          clearInterval(timer);
+          setShowAlert(false);
+          return 0;
+        }
+        return nextValue;
+      });
+    }, interval);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <>
+      {showAlert && (
+        <Box
+          width={"50%"}
+          height={"300px"} // Fixing the height so that the space is always reserved
+          position="relative"
+          margin="auto"
+          display="block"
+          marginBottom={"10px"}
+        >
+          <Alert
+            display={"flex"}
+            width={"100%"}
+            status="error"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+          >
+            <AlertIcon />
+            <AlertTitle mr={2}>Final Warning</AlertTitle>
+            <AlertDescription flex="1">
+              Unusual activity and/or suspicious behaviour has been detected.
+              <br />
+              This is your last chance to complete your exam and avoid exam termination.
+            </AlertDescription>
+          </Alert>
+          <Progress
+            size="xs"
+            value={(progress / 3000) * 100} // Adjust the denominator to your total duration
+            position="relative"
+            width="100%"
+            bottom="0"
+            colorScheme="red"
+          />
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default WarningTwo;
