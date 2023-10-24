0181a60876cb78b50936694efc5a9354d1ff1dfd
zetovicmarko1 <zetovicmarko1@gmail.com>
2023-10-24 23:01:56 +1100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production'
-  ? '/frontend_core/'
+  ? '/frontend_core'
   : '/'
 })
 
