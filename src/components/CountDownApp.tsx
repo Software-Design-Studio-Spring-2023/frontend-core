af1b9a9cd44bda369b869821c395c03e63e9f0e6
zetovicmarko1 <zetovicmarko1@gmail.com>
2023-10-27 11:48:23 +1100

    timer does not start until currentExam.has_started

--- src/components/CountDownApp.tsx
+++ src/components/CountDownApp.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useCountdown } from "../hooks/useCountdown";
 import { Heading } from "@chakra-ui/react";
 
@@ -18,9 +18,16 @@ const timeTakeaway = stamp - currentExam.time_started;
 const initialTotalTimeMS = 3600000; //2 Hours
 
 export const CountDownApp = () => {
-  const timeMS = useCountdown(initialTotalTimeMS, () =>
-    console.log("Times up!!")
-  );
+  const [hasStarted, setHasStarted] = useState(currentExam.has_started);
+
+  useEffect(() => {
+    // Update hasStarted state whenever currentExam.has_started changes
+    setHasStarted(currentExam.has_started);
+  }, [currentExam.has_started]);
+
+  const timeMS = hasStarted
+    ? useCountdown(initialTotalTimeMS, () => console.log("Times up!!"))
+    : initialTotalTimeMS;
 
   let timeTotalSeconds = timeMS / 1000;
   let timeTotalMinutes = Math.floor(timeTotalSeconds / 60);
